aebe2af19ceea80a5139781d0eb81963bca68ef9
Eric <emjyoon@gmail.com>
2021-04-02 13:10:56 -0700

    Toggle channel if no argument is specified

--- AutoTeamChat/src/main/java/com/yoonicode/autoteamchat/PluginCommands.java
+++ AutoTeamChat/src/main/java/com/yoonicode/autoteamchat/PluginCommands.java
@@ -20,14 +20,14 @@ public class PluginCommands implements CommandExecutor {
     };
     public final PluginMain main;
 
-    public PluginCommands(PluginMain main){
+    public PluginCommands(PluginMain main) {
         this.main = main;
     }
 
-    public List<String> getCompletions(String[] args, List<String> existingCompletions){
+    public List<String> getCompletions(String[] args, List<String> existingCompletions) {
 
-        if(args[0].equals("chat")){
-            return new ArrayList<String>(){
+        if (args[0].equals("chat")) {
+            return new ArrayList<String>() {
                 {
                     add("team");
                     add("all");
@@ -37,52 +37,68 @@ public class PluginCommands implements CommandExecutor {
         return existingCompletions;
     }
 
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args){
-        if(label.equals("chat") || label.equals("autoteamchat:chat")){
-            if(!(commandSender instanceof Player)){
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if (label.equals("chat") || label.equals("autoteamchat:chat")) {
+            if (!(commandSender instanceof Player)) {
                 commandSender.sendMessage("Only players can use this command.");
                 return false;
             }
-            if(!commandSender.hasPermission("minecraft.command.teammsg") && !commandSender.isOp()){
+            if (!commandSender.hasPermission("minecraft.command.teammsg") && !commandSender.isOp()) {
                 commandSender.sendMessage("You don't have permission to run this command!");
                 return false;
             }
-            if(args.length < 0){
-                commandSender.sendMessage("Specify what channel to change to. Usage:\n " + ChatColor.AQUA + "/chat team\n/chat all");
-                return false;
-            }
-            Player player = (Player)commandSender;
+            Player player = (Player) commandSender;
             UUID uid = player.getUniqueId();
-            if(args[0].equalsIgnoreCase("all") || args[0].equalsIgnoreCase(("a"))){
-                if(!main.teamChatPlayers.contains(uid)){
-                    commandSender.sendMessage("You are already in all chat!");
-                    return true;
+            if (args.length == 0) {
+                if (main.teamChatPlayers.contains(uid)) {
+                    joinAllChat(player);
+                } else {
+                    joinTeamChat(player);
                 }
-                main.teamChatPlayers.remove(uid);
-                commandSender.sendMessage("You are now in " + ChatColor.AQUA + "ALL" + ChatColor.RESET + " chat");
                 return true;
-            } else if(args[0].equalsIgnoreCase("team") || args[0].equalsIgnoreCase(("t"))){
-                if(main.teamChatPlayers.contains(uid)){
-                    commandSender.sendMessage("You are already in team chat!");
-                    return true;
-                }
-                if(main.scoreboard.getEntryTeam(player.getName()) == null){
-                    commandSender.sendMessage("You're not on a team! Join a team before using team chat.");
-                    return true;
-                }
-                main.teamChatPlayers.add(uid);
-                commandSender.sendMessage("You are now in " + ChatColor.GREEN + "TEAM" + ChatColor.RESET + " chat");
+            }
+            if (args[0].equalsIgnoreCase("all") || args[0].equalsIgnoreCase(("a"))) {
+                joinAllChat(player);
+                return true;
+            } else if (args[0].equalsIgnoreCase("team") || args[0].equalsIgnoreCase(("t"))) {
+                joinTeamChat(player);
                 return true;
             } else {
                 commandSender.sendMessage("That is not a valid chat channel! Usage:\n " + ChatColor.AQUA + "/chat team\n/chat all");
                 return false;
             }
         }
-        if(label.equals("reload") || label.equals("autoteamchat:reload")){
+        if (label.equals("reload") || label.equals("autoteamchat:reload")) {
             main.reloadFromConfig();
             commandSender.sendMessage("Config reloaded!");
             return true;
         }
         return false;
+    }
+
+    void joinAllChat(Player player){
+        UUID uid = player.getUniqueId();
+        if (!main.teamChatPlayers.contains(uid)) {
+            player.sendMessage("You are already in all chat!");
+            return;
+        }
+        main.teamChatPlayers.remove(uid);
+        player.sendMessage("You are now in " + ChatColor.AQUA + "ALL" + ChatColor.RESET + " chat");
+        return;
+    }
+
+    void joinTeamChat(Player player){
+        UUID uid = player.getUniqueId();
+        if(main.teamChatPlayers.contains(uid)){
+            player.sendMessage("You are already in team chat!");
+            return;
+        }
+        if(main.scoreboard.getEntryTeam(player.getName()) == null){
+            player.sendMessage("You're not on a team! Join a team before using team chat.");
+            return;
+        }
+        main.teamChatPlayers.add(uid);
+        player.sendMessage("You are now in " + ChatColor.GREEN + "TEAM" + ChatColor.RESET + " chat");
+        return;
     }
 }
